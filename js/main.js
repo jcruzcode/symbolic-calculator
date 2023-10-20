c726499d0822ca53f58a2d3e2272e357c2d065f5
jcruzcode <jcruz.code@gmail.com>
2023-10-19 20:46:33 -0800

    Updated main.js for pop-up menu

--- js/main.js
+++ js/main.js
@@ -60,3 +60,21 @@ function calculate() {
     });
 }
 
+// Modal JS
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("advancedFunctions");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
